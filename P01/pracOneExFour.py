482a1d7b8c01091060477c9c776b1536bd304f42
Jonathan Wright <techypanda@hotmail.com>
2020-08-13 14:22:30 +0800

    Updated to use no hardcoded values for rotation

--- P01/pracOneExFour.py
+++ P01/pracOneExFour.py
@@ -1,12 +1,24 @@
 import cv2 as cv
 import numpy as np
+import math
 
 def main():
     image = cv.imread("./in_images/prac01ex04img01.png", cv.IMREAD_COLOR)
+    print(image[0:0, 0:0])
     rows, cols, color = image.shape
-    transformMatrix = cv.getRotationMatrix2D((rows/2, cols/2), 45, 1)
+    p1, p2, p0 = 0, 0, (0,0)
+    # FIND HORIZONTAL POINT ONE.
+    for i in range(0,image.shape[1]):
+        if (image[i,0] != np.array([0,0,0])).all():
+            p1 = (i, 0)
+    # FIND VERTICAL
+    for i in range(0, image.shape[0]):
+        if (image[0,i] != np.array([0,0,0])).all():
+            p2 = (0, i)
+
+    d = int(round(math.degrees(math.atan(p1[0] / p2[1]))))
+    transformMatrix = cv.getRotationMatrix2D((rows/2, cols/2), d, 1)
     rotatedImage = cv.warpAffine(image, transformMatrix, (cols, rows))
-    cv.imshow('Unrotated', image)
     cv.imshow('Rotated', rotatedImage)
     cv.waitKey()
     print("Writing To File!")
