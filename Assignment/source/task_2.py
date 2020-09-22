39dcc7321059fd98baed0228426a85a9893d3932
Techypanda <Jonathan_wright@hotmail.com>
2020-09-22 17:39:08 +0800

    a

--- Assignment/source/task_2.py
+++ Assignment/source/task_2.py
@@ -2,29 +2,32 @@ import cv2 as cv
 import numpy as np
 import utils
 import math
-
-title = {'./images/diamond2.png': 'diamond', './images/Dugong.jpg': 'Dugong'} # lazy quick fix...
-imageSources = ['./images/diamond2.png', './images/Dugong.jpg']
-for imageSource in imageSources: # ROTATION
-  images = { 
-    'neutral': [cv.imread(imageSource, cv.IMREAD_COLOR)],
-    'deg45': [utils.rAngle(cv.imread(imageSource, cv.IMREAD_COLOR), math.radians(45))],
-    'degn45': [utils.rAngle(cv.imread(imageSource, cv.IMREAD_COLOR), math.radians(-45))],
-    'deg90': [cv.rotate(cv.imread(imageSource, cv.IMREAD_COLOR), cv.ROTATE_90_CLOCKWISE)],
-    'degn90': [cv.rotate(cv.imread(imageSource, cv.IMREAD_COLOR), cv.ROTATE_90_COUNTERCLOCKWISE)],
-    'flipped': [cv.flip(cv.imread(imageSource, cv.IMREAD_COLOR), 0)]
-  }
-  # Chosen Feature Is The Number 2 in card.
-  hog = cv.HOGDescriptor('hog.xml')
-  test = images['neutral'][0][5:69, 0:64]
-  cv.imshow('crop', test)
-  cv.waitKey()
-
-for imageSource in imageSources:
-  images = { 
-    'neutral': [cv.imread(imageSource, cv.IMREAD_COLOR)],
-    '75%': [utils.scaleImg(cv.imread(imageSource, cv.IMREAD_COLOR), 0.75, 0.75)],
-    '50%': [utils.scaleImg(cv.imread(imageSource, cv.IMREAD_COLOR), 0.50, 0.50)],
-    'widthReduced': [utils.scaleImg(cv.imread(imageSource, cv.IMREAD_COLOR), 0.50, 1)],
-    'heightReduced': [utils.scaleImg(cv.imread(imageSource, cv.IMREAD_COLOR), 1, 0.50)]
-  }
+from matplotlib import pyplot as plt
+# 860 x 480 480 x 274
+hog = cv.HOGDescriptor('hog.xml')
+winStride = (8,8)
+padding = (8,8)
+locations = ((0,0),)
+card = cv.imread('./images/diamond2.png', cv.IMREAD_GRAYSCALE)
+card90 = cv.rotate(card, cv.ROTATE_90_CLOCKWISE)
+cardn90 = cv.rotate(card, cv.ROTATE_90_COUNTERCLOCKWISE)
+card180 = cv.rotate(card, cv.ROTATE_180)
+dugong = cv.imread('./images/Dugong.jpg', cv.IMREAD_GRAYSCALE)
+card = card[5:69, 0:64] # crop to 64x64 number 2 feature.
+dugong = dugong[209:209+64, 390:390+64] # crop to 64x64 dugong feature. 
+dugong90 = cv.rotate(dugong, cv.ROTATE_90_CLOCKWISE)
+dugongn90 = cv.rotate(dugong, cv.ROTATE_90_COUNTERCLOCKWISE)
+dugong180 = cv.rotate(dugong, cv.ROTATE_180)
+cv.imshow('card', card)
+cv.imshow('dugong', dugong)
+cardHists = [
+  (hog.compute(card,winStride,padding,locations), './out_files/task_2/rotation/noRotation-card.png'), (hog.compute(card90,winStride,padding,locations), './out_files/task_2/rotation/90deg-card.png'), 
+  (hog.compute(cardn90,winStride,padding,locations), './out_files/task_2/rotation/n90deg-card.png'), (hog.compute(card180,winStride,padding,locations), './out_files/task_2/rotation/180deg-card.png')
+]
+dugongHists = [
+  hog.compute(dugong,winStride,padding,locations), hog.compute(dugong90,winStride,padding,locations),
+  hog.compute(dugongn90,winStride,padding,locations), hog.compute(dugong180,winStride,padding,locations)
+]
+for hist in cardHists:
+  plt.plot(hist[0])
+  plt.savefig(hist[1])
