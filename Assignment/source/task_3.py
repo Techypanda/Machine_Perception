1e01d613c8a7a8673336da9f7518f00e96d7c4a7
Techypanda <Jonathan_wright@hotmail.com>
2020-09-23 17:11:59 +0800

    Before Revert

---
+++ Assignment/source/task_3.py
@@ -0,0 +1,112 @@
+import cv2 as cv
+import numpy as np
+import utils
+import random
+
+card = cv.imread("./images/diamond2.png", cv.IMREAD_GRAYSCALE)
+dugong = cv.imread('./images/Dugong.jpg', cv.IMREAD_GRAYSCALE)
+blockSize = 11
+inbuiltBlur = 5
+dugong = cv.adaptiveThreshold(dugong,255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, blockSize, inbuiltBlur)
+ker = np.ones((10,10), np.uint8)
+dugong = cv.morphologyEx(dugong, cv.MORPH_OPEN, ker)
+ker = np.ones((4,4), np.uint8)
+dugong = cv.morphologyEx(dugong, cv.MORPH_CLOSE, ker)
+cv.imshow('dugong', dugong)
+cv.waitKey()
+_, card = cv.threshold(card, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+card = cv.bitwise_not(card)
+ret, labels = cv.connectedComponents(card)
+count = 1
+
+for label in range(1,ret):
+    mask = np.array(labels, dtype=np.uint8)
+    mask[labels == label] = 255
+    yLoc = None # min max
+    xLoc = None # min max
+    for y in range(mask.shape[0]):
+      for x in range(mask.shape[1]):
+        if mask[y,x] == 255:
+          if yLoc == None:
+            yLoc = [y,y]
+            xLoc = [x,x]
+          if y < yLoc[0]: # min
+            yLoc[0] = y
+          elif y > yLoc[1]:
+            yLoc[1] = y
+          if x < xLoc[0]:
+            xLoc[0] = x
+          elif x > xLoc[1]:
+            xLoc[1] = x
+    cv.imwrite('./out_files/task_3/card_objects/DetectedObject-{}.png'.format(count), 
+      (cv.imread('./images/diamond2.png', cv.IMREAD_COLOR))[yLoc[0]:yLoc[1],xLoc[0]:xLoc[1]]
+    )
+    cv.destroyAllWindows()
+    count += 1
+
+dugong = cv.bitwise_not(dugong)
+ret, labels = cv.connectedComponents(dugong)
+count = 1
+for label in range(1,ret):
+    mask = np.array(labels, dtype=np.uint8)
+    mask[labels == label] = 255
+    yLoc = None # min max
+    xLoc = None # min max
+    for y in range(mask.shape[0]):
+      for x in range(mask.shape[1]):
+        if mask[y,x] == 255:
+          if yLoc == None:
+            yLoc = [y,y]
+            xLoc = [x,x]
+          if y < yLoc[0]: # min
+            yLoc[0] = y
+          elif y > yLoc[1]:
+            yLoc[1] = y
+          if x < xLoc[0]:
+            xLoc[0] = x
+          elif x > xLoc[1]:
+            xLoc[1] = x
+    cv.imwrite('./out_files/task_3/dugong_objects/DetectedObject-{}.png'.format(count), 
+      (cv.imread('./images/Dugong.jpg', cv.IMREAD_COLOR))[yLoc[0]:yLoc[1],xLoc[0]:xLoc[1]]
+    )
+    cv.destroyAllWindows()
+    count += 1
+'''
+dugong = utils.imgToBinary(dugong)
+card = utils.imgToBinary(card)
+utils.toFile(dugong, './out_files/task_3/dugong_objects/dugong.txt')
+utils.toFile(card, './out_files/task_3/card_objects/card_before.txt') # will look strange but it is correct
+
+utils.concon(dugong); utils.concon(card)
+
+utils.toFile(dugong, './out_files/task_3/dugong_objects/dugong_objects.txt')
+utils.toFile(card, './out_files/task_3/card_objects/card_objects.txt') # will look strange but it is correct
+dugongObjs = utils.conObjects(dugong)
+cardObjs = utils.conObjects(card)
+objs = [(dugongObjs, './images/Dugong.jpg', './out_files/task_3/dugong_objects/detectedObjectsCCL.png', './out_files/task_3/dugong_objects/'),(cardObjs, './images/diamond2.png', './out_files/task_3/card_objects/detectedObjectsCCL.png', './out_files/task_3/card_objects/')]
+for obj in objs:
+  clr = cv.imread(obj[1], cv.IMREAD_COLOR)
+  for key, coords in obj[0].items():
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    for cord in coords:
+      clr = cv.rectangle(clr, (cord[1], cord[0]), (cord[1], cord[0]), color)
+  cv.imwrite(obj[2], clr)
+
+  # Extract The Objects Into Seperate Files
+  objectNo = 1
+  for key, coords in obj[0].items():
+    xMin = coords[0][1]; xMax = coords[0][1]
+    yMin = coords[0][0]; yMax = coords[0][0]
+    for coord in coords:
+      if coord[1] < xMin:
+        xMin = coord[1]
+      elif coord[1] > xMax:
+        xMax = coord[1]
+      if coord[0] < yMin:
+        yMin = coord[0]
+      elif coord[0] > yMax:
+        yMax = coord[0]
+    out = cv.imread(obj[1], cv.IMREAD_COLOR)
+    out = out[yMin:yMax, xMin:xMax]
+    cv.imwrite("{}DetectedObject-{}.png".format(obj[3], objectNo), out)
+    objectNo += 1'''
--- Assignment/source/utils.py
+++ Assignment/source/utils.py
@@ -36,3 +36,91 @@ def placeOn(img, desiredWidth, desiredHeight):
   canvas = np.zeros((desiredHeight, desiredWidth), dtype=np.uint8)
   canvas[0:img.shape[0], 0:img.shape[1]] = img
   return canvas
+
+def toFile(arr, filename):
+  with open(filename, 'w') as file:
+    for y in range(len(arr)):
+      for x in range(len(arr[y])):
+        file.write(str(arr[y][x]))
+      file.write('\n')
+
+def concon(arr):
+  conflicts = {} # dict
+  cc = 0
+  for y in range(len(arr)):
+    for x in range(len(arr[y])):
+      if arr[y][x] == 1:
+        if y > 0:
+          if x > 0:
+            if arr[y][x-1] != 0:
+              if arr[y-1][x] != 0:
+                if arr[y-1][x] != arr[y][x-1]:
+                  if conflicts.get(arr[y-1][x]) != None: #TOP
+                    conflicts[arr[y-1][x]].add(arr[y][x-1]) # add left
+                    conflicts[arr[y-1][x]].add(arr[y-1][x]) # add up
+                  else:
+                    z = set()
+                    z.add(arr[y][x-1]) # add left
+                    z.add(arr[y-1][x]) # add up
+                    conflicts[arr[y-1][x]] = z
+                  if conflicts.get(arr[y][x-1]) != None: # LEFT
+                    conflicts[arr[y][x-1]].add(arr[y-1][x]) # add up
+                    conflicts[arr[y][x-1]].add(arr[y][x-1]) # add left
+                  else:
+                    z = set()
+                    z.add(arr[y-1][x]) # add up
+                    z.add(arr[y][x-1]) # add left
+                    conflicts[arr[y][x-1]] = z
+                  # merge the sets.
+                  conflicts[arr[y-1][x]].update(conflicts[arr[y][x-1]])
+                  conflicts[arr[y][x-1]].update(conflicts[arr[y-1][x]])
+                arr[y][x] = arr[y][x-1]
+              else:
+                arr[y][x] = arr[y][x-1]
+            elif arr[y-1][x] != 0:
+              arr[y][x] = arr[y-1][x]
+            else: #not connected
+              cc += 1
+              arr[y][x] = cc
+          elif arr[y-1][x] != 0:
+            arr[y][x] = arr[y-1][x]
+          else: # not connected
+            cc += 1
+            arr[y][x] = cc
+        elif x > 0:
+          if arr[y][x-1] != 0:
+            arr[y][x] = arr[y][x-1]
+          else: # not connected
+            cc += 1
+            arr[y][x] = cc
+        else: # not connected
+          cc += 1
+          arr[y][x] = cc
+      elif arr[y][x] == 0:
+        arr[y][x] = 0
+  for y in range(len(arr)):
+    for x in range(len(arr[y])):
+      if conflicts.get(arr[y][x]) != None:
+        arr[y][x] = min(conflicts[arr[y][x]])
+
+def conObjects(arr):
+  uniqueObjs = {}
+  for y in range(len(arr)):
+    for x in range(len(arr[y])):
+      if uniqueObjs.get(arr[y][x]) != None:
+        uniqueObjs[arr[y][x]].append((y,x))
+      else:
+        new = []
+        new.append((y,x))
+        uniqueObjs[arr[y][x]] = new
+  return uniqueObjs
+
+def imgToBinary(img):
+  binOutput = np.zeros(shape = (img.shape[0], img.shape[1]), dtype=np.int)
+  for y in range(img.shape[0]):
+    for x in range(img.shape[1]):
+      if img[y, x] == 255:
+        binOutput[y][x] = 0
+      else:
+        binOutput[y][x] = 1
+  return binOutput
